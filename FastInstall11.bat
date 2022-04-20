62ba449ffabb65af4aa6f0d4d486ab823ae5f5ba
TheHerjei <gancio93@gmail.com>
2022-04-20 12:44:17 +0200

    folder structure

--- FastInstall11.bat
+++ FastInstall11.bat
@@ -22,6 +22,9 @@ IF %ERRORLEVEL% EQU 0 (
 	icacls E:\autoRun.Inf /deny everyone:F
 )
 
+REM Aggiungo esclusione per C:\bin da Win Defender
+powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath "C:\bin"
+
 REM Copio i tools in C:\bin
 mkdir %HOMEDRIVE%\bin
 robocopy /mt:128 /S /E /R:0 /W:0 %BASEPATH%\bin %HOMEDRIVE%\bin
@@ -29,34 +32,39 @@ robocopy /mt:128 /S /E /R:0 /W:0 %BASEPATH%\bin %HOMEDRIVE%\bin
 REM Imposto C:\bin sulla variabile PATH...
 setx /M PATH "%PATH%;C:\bin"
 
-REM Installo Chocolatey
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+REM Disinstallo applicazioni inutili
+REM winget uninstall -h Microsoft.OneDrive
+REM winget uninstall -h Microsoft.Edge
+REM winget uninstall -h "Microsoft Edge Update"
+REM winget uninstall -h Microsoft.EdgeWebView2Runtime
 
-REM Tools
-choco install -y 7zip
-choco install -y notepadplusplus
-choco install -y wiztree
-choco install -y naps2
-choco install -y adobereader
-
-REM Net/Sys tools
-choco install -y FileZilla
-
-REM Audio/Video
-choco install -y VideoLAN.VLC
-choco install -y irfanview irfanviewplugins
-choco install -y audacity
-
-REM Office
-choco install -y Google.Chrome
-choco install -y pdfsam pdf24
-choco install -y libreoffice
+REM Installo applicazioni
+winget upgrade --all --accept-source-agreements
+winget install -e -h VideoLAN.VLC
+winget install -e -h Notepad++.Notepad++
+winget install -e -h 7zip.7zip
+REM winget install -e -h Zoom.Zoom
+winget install -e -h Google.Chrome
+REM winget install -e -h Adobe.Acrobat.Reader.64-bit
+REM winget install -e -h AntibodySoftware.WizTree
+REM winget install -e -h TheDocumentFoundation.LibreOffice
+REM winget install -e -h PDFsam.PDFsam
+REM winget install -e -h voidtools.Everything
+winget install -e -h Mozilla.Firefox
+winget install -e -h Cyanfish.NAPS2
 
 REM Link di Aiuto sul desktop
-mklink %PUBLIC%\Desktop\AIUTO "C:\Windows\Sytem32\quickassist.exe"
+REM mklink %PUBLIC%\Desktop\AIUTO "C:\Windows\System32\quickassist.exe"
 
-REM Escludo l'ibernazione...
+REM Opzioni di risparmio energia
+REM Escludo l'ibernazione... (da usare su pc fissi)
 powercfg /hibernate off
+
+REM Imposto timeout disattivazione schermo
+powercfg /change monitor-timeout-ac 10
+
+REM Disabilito sospensione automatica se alimentato a rete elettrica
+powercfg /change standby-timeout-ac 0
 
 REM Disattivo servizi non utili
 sc config "DusmSvc" start= disabled
@@ -69,7 +77,7 @@ sc config "iphlpsvc" start= disabled
 sc config "WdiServiceHost" start= disabled
 sc config "WdiSystemHost" start= disabled
 sc config "AppReadiness" start= disabled
-sc config "LanmanServer" start= demand
+REM sc config "LanmanServer" start= demand
 sc config "DPS" start= disabled
 sc config "RetailDemo" start= disabled
 sc config "WbioSrvc" start= disabled
@@ -83,7 +91,6 @@ sc config "UevAgentService" start= disabled
 sc config "SQLWriter" start= disabled
 sc config "SysMain" start= disabled
 sc config "TapiSrv" start= disabled
-sc config "Themes" start= disabled
 sc config "WalletService" start= disabled
 sc config "WSearch" start= disabled
 sc config "XboxGipSvc" start= disabled
@@ -105,13 +112,39 @@ sc config "PhoneSvc" start= disabled
 sc config "wisvc" start= disabled
 sc config "ZoomCptService" start= manual
 
+REM Attivo utente Administrator ATTENZIONE! USARE CON CAUTELA!
+REM net user Administrator admin /active:yes
+
 REM Importo l'associazione files per tutti gli utenti
-if EXIST %BASEPATH%\AppAssociations\AppAssociations.xml (
+if EXIST %BASEPATH%\AppAssociations.xml (
 dism /online /Import-DefaultAppAssociations:"%BASEPATH%\AppAssociations\AppAssociations.xml"
 )
+
+REM Disabilito Consumer Experience
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows\CloudContent /v DisableWindowsConsumerFeatures /t REG_DWORD /d 1 /f
+
+REM Creo pianificazione per upgrade automatico attraverso winget
+SCHTASKS /CREATE /SC DAILY /TN "AppUpgrade" /TR "C:\bin\appupgrade.bat" /ST 18:00
+
+REM Rimuovo pianificazioni esistenti non insteressanti
+SCHTASKS /DELETE /TN "Adobe Acrobat Update Task" /F
+REM SCHTASKS /DELETE /TN "GoogleUpdade*" /F
+REM SCHTASKS /DELETE /TN "MicrosoftEdge*" /F
 
 REM Rimuovo AppXProvisionedPackage per tutti gli utenti
 powershell Set-ExecutionPolicy Bypass -Scope Process -Force; %BASEPATH%\appxRemove.ps1
 
-start C:\dism++x64.exe
-start C:\WPD.exe
+REM Abilito le connessioni desktop remoto multiple
+REM **Questo script di powershell va rilanciato dopo ogni feature upgrade di windows**
+powershell Set-ExecutionPolicy Bypass -Scope Process -Force; %BASEPATH%\multiplerdp.ps1
+
+REM Rimuovo Wallpaper
+REM REG ADD "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "" /f
+REM RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+
+REM Rinomino il computer
+wmic ComputerSystem where Name=%COMPUTERNAME% Rename Name="UFFTC01"
+
+REM Apro WPD e Dism per configurazioni aggiuntive
+start C:\bin\WPD.exe
+start C:\bin\Dism++x64.exe
--- old/choco-install.bat
+++
@@ -1,4 +0,0 @@
-powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-IF ERRORLEVEL == 1 (echo # L'installazione non è andata a buon fine...)
-IF ERRORLEVEL == 0 (echo # Installazione completata con successo!)
